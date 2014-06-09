d9450ec9031246cc9b37c311485339997194a1e9
Bob <christian.erhardt@mojo2k.de>
2014-06-09 21:32:54 +0200

    initial

---
+++ NunitTest/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NunitTest
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCaseSource(typeof(MySource), "TestDataVonBisIdentisch")]
+        public void ZeitraumTests(IEnumerable<Zeitraum> testCases)
+        {}
+    }
+
+    public class MySource : TestCaseData
+    {
+        private static IEnumerable TestDataVonBisIdentisch()
+        {
+            yield return new TestCaseData(new[]
+                {
+                    new Zeitraum(DateTime.Now),
+                    new Zeitraum(DateTime.Now),
+                    new Zeitraum(DateTime.Now),
+                });
+        }
+    }
+
+    public struct Zeitraum : IComparable
+    {
+        public Zeitraum(DateTime? date)
+        {}
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
